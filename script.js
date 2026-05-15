4255b49e067c51fa7024deac57c42de78afbf408
Billz-avtamartizatsiya <abdurashidov0408@gmail.com>
2026-05-15 20:09:14 +0500

    Fix token formatting in script.js

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ function handleSubmit(event) {
 🏪 Do‘kon: ${shop}
 💬 Xabar: ${msg}`;
 
-  let token = "8866953084:AAExmoeRPGBPUCrbNos-0fVR-FJW-GpgJL0
-";
+  let token = "8866953084:AAExmoeRPGBPUCrbNos-0fVR-FJW-GpgJL0";
   let chat_id = "5206100660";
 
   fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
